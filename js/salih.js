cedeac2018e45dfccf53477d9c8eebbf613ce9b8
SalihBulmaz <bulmaz19@itu.edu.tr>
2023-01-04 17:09:29 +0300

    quick changes on css

--- js/salih.js
+++ js/salih.js
@@ -1,7 +1,7 @@
-const signin_form = document.getElementById('form-signin')
-const signup_form = document.getElementById('form-signup')
-const show_signin = document.getElementById('show-signin')
-const show_signup = document.getElementById('show-signup')
+const signin_form = document.querySelector('.form-signin')
+const signup_form = document.querySelector('.form-signup')
+const show_signin = document.querySelector('.show-signin')
+const show_signup = document.querySelector('.show-signup')
 
 signin_form.addEventListener('submit', function(e) {
     e.preventDefault()
@@ -10,18 +10,11 @@ signin_form.addEventListener('submit', function(e) {
 show_signup.addEventListener('click', (e) => {
     e.preventDefault()
     signin_form.style.display = 'none'
-    signup_form.style.display = 'contents'
-    console.log('show signup')
+    signup_form.style.display = 'block'
 })
 
 show_signin.addEventListener('click', (e) => {
     e.preventDefault()
     signin_form.style.display = 'block'
     signup_form.style.display = 'none'
-    console.log('show signin')
-})
-
-const labels = document.querySelectorAll('label')
-labels.forEach((elm) => {
-    elm.style.marginTop = '1.2rem'
 })
