c9f93a19099730d1541913a81c472bb5b991223e
SalihBulmaz <bulmaz19@itu.edu.tr>
2023-01-04 15:02:45 +0300

    initial commit

---
+++ js/salih.js
@@ -0,0 +1,20 @@
+const signin_form = document.getElementById('signin-form')
+// const signup_form = document.getElementById('signup-form')
+// const show_signup = document.getElementById('show-signup')
+// const show_signin = document.getElementById('show-signin')
+// var formDisplayed = 1
+
+signin_form.addEventListener('submit', function(e) {
+    e.preventDefault()
+})
+// show_signup.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     formDisplayed = 2
+//     console.log('signup displayed', formDisplayed)
+// })
+
+// show_signin.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     formDisplayed = 2
+//     console.log('signup displayed', formDisplayed)
+// })
