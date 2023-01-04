43ca1b7a344738c09b5471db96b728caa7e14282
SalihBulmaz <bulmaz19@itu.edu.tr>
2023-01-04 16:49:51 +0300

    signup form added

--- js/salih.js
+++ js/salih.js
@@ -1,20 +1,27 @@
-const signin_form = document.getElementById('signin-form')
-// const signup_form = document.getElementById('signup-form')
-// const show_signup = document.getElementById('show-signup')
-// const show_signin = document.getElementById('show-signin')
-// var formDisplayed = 1
+const signin_form = document.getElementById('form-signin')
+const signup_form = document.getElementById('form-signup')
+const show_signin = document.getElementById('show-signin')
+const show_signup = document.getElementById('show-signup')
 
 signin_form.addEventListener('submit', function(e) {
     e.preventDefault()
 })
-// show_signup.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     formDisplayed = 2
-//     console.log('signup displayed', formDisplayed)
-// })
 
-// show_signin.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     formDisplayed = 2
-//     console.log('signup displayed', formDisplayed)
-// })
+show_signup.addEventListener('click', (e) => {
+    e.preventDefault()
+    signin_form.style.display = 'none'
+    signup_form.style.display = 'contents'
+    console.log('show signup')
+})
+
+show_signin.addEventListener('click', (e) => {
+    e.preventDefault()
+    signin_form.style.display = 'block'
+    signup_form.style.display = 'none'
+    console.log('show signin')
+})
+
+const labels = document.querySelectorAll('label')
+labels.forEach((elm) => {
+    elm.style.marginTop = '1.2rem'
+})
